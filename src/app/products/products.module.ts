58359097d6394ee2c1e02179c78f150135453802
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-19 15:38:37 +0200

    traditional reactive form with custom directive - to optimize the code - exercise 312

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
@@ -11,7 +12,8 @@ import { ProductPageComponent } from './pages/product-page/product-page.componen
   ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductsModule { }
