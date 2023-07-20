6d9ddb88686d19808a3ae36e7add23f3fee3da47
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-20 15:24:49 +0200

    signals - user information - exercise 325

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './user-info-page.component.html',
   styleUrls: ['./user-info-page.component.css']
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+
+  private userService = inject(UsersServiceService);
+  public userId = signal(1);
+
+  public currentUser = signal<User|undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser( this.userId() )
+  }
+
+  loadUser( id: number ){
+    if ( id <= 0 ) return; 
+
+    this.userId.set(id); //si el valor es mayor a 0 lo conecto con el id
+    this.currentUser.set(undefined);
+  
+    this.userService.getUserById( id )
+      .subscribe( user => {
+        this.currentUser.set( user )
+      })
+  }
+
 
 }
